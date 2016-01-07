a176585986a96fdd065ae350e1635d0e4d3a8fa1
Thomas Wilburn <twilburn@seattletimes.com>
2016-01-07 14:55:55 -0800

    Add readme with last year's lessons learned.

--- monitor.js
+++ monitor.js
@@ -2,6 +2,7 @@ var async = require("async");
 var request = require("request");
 var crypto = require("crypto");
 var chalk = require("chalk");
+var spawn = require("child_process").spawn;
 
 var pages = {
   king: "http://your.kingcounty.gov/elections/2015/nov-general/results/pi.txt",
@@ -20,23 +21,29 @@ var check = function() {
     request({ uri: url }, function(err, response, body) {
       var md5 = crypto.createHash("md5").update(body, "utf8");
       var hash = md5.digest("hex");
+      var updated = false;
       // console.log(key, hash);
       if (!results[key]) {
         console.log("Adding cache for %s - %s", key, hash);
       } else {
         if (results[key] !== hash) {
           console.log(chalk.red("!!!!!!!! UPDATE ON %s !!!!!!!!!"), key);
-          process.exit();
+          updated = true;
         } else {
-          console.log(chalk.green("no change on %s - %s"), key, hash)
+          console.log(chalk.green("no change on %s - %s"), key, hash);
         }
       }
       results[key] = hash;
-      c();
+      c(updated);
     });
-  }, function() {
+  }, function(err) {
+    if (err) {
+      var child = spawn("grunt", "sheets static publish:live".split(" "), { stdio: "inherit" });
+      child.on("close", function() {
+        setTimeout(check, 1000 * 60 * 2);
+      });
+    } else setTimeout(check, 1000 * 60 * 2);
     console.log("============\n");
-    setTimeout(check, 1000 * 60 * 2);
   });
 };
 
